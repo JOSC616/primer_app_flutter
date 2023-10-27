182d306db449c0a262a17a36783bc1868082cd93
joustin <jsalazchong@gmail.com>
2023-10-27 12:13:51 -0700

    proyecto con estilos

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 140, 255)),
           useMaterial3: true,
         ),
         home: const MyHomePage(),
@@ -23,30 +23,60 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class MyAppState extends ChangeNotifier{
+
+class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-  void getSiguiente(){
-      current = WordPair.random();
-      notifyListeners();
+  void getSiguiente() {
+    current = WordPair.random();
+    notifyListeners();
   }
 }
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     return Scaffold(
-      body: Column(
-        children: [
-          Text("idea"),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: (){
-              appState.getSiguiente();
-            },
-            child: Text("Siguiente")),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("idea"),
+            BigCard(idea: appState.current),
+            SizedBox(height: 100.0,),
+            ElevatedButton(
+                onPressed: () {
+                  appState.getSiguiente();
+                },
+                child: Text("Siguiente")),
+          ],
+        ),
       ),
     );
   }
+}
+
+class BigCard extends StatelessWidget {
+  final WordPair idea;
+  const BigCard({super.key, required this.idea});
+
+  @override
+  Widget build(BuildContext context) {
+    final tema = Theme.of(context);
+    final textStyle = tema.textTheme.displayMedium!.copyWith(
+      color: tema.colorScheme.onPrimary,
+    );
+    return Card(
+      color: tema.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          idea.asLowerCase, 
+          style: textStyle,
+          semanticsLabel: "${idea.first} ${idea.second}",
+        ),
+      ),
+    );
   }
+}
